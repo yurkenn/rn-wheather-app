210e4e6696a608063c5e072a0e12ed2408322cdb
Oguz Yurken <oguz.yurken@gmail.com>
2023-01-22 04:09:45 +0300

    Update Home Screen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,15 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-
+import Input from '../../components/Input';
+import useFetch from '../../hooks/useFetch/useFetch';
+import Config from 'react-native-config';
 const Home = () => {
+  const {data, loading, error, setCityName} = useFetch(Config.API_URL);
+  console.log(data);
+
   return (
     <View>
-      <Text>Home</Text>
+      <Input placeholder={'Enter City Name'} />
     </View>
   );
 };
