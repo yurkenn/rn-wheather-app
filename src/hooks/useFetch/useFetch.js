e5222ae0defe75e54701e637ac0d73dc4bec0ade
Oguz Yurken <oguz.yurken@gmail.com>
2023-01-22 04:00:27 +0300

    Update useFetch

--- src/hooks/useFetch/useFetch.js
+++ src/hooks/useFetch/useFetch.js
@@ -1,18 +1,26 @@
 import {View, Text} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Config from 'react-native-config';
 
 const useFetch = url => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [cityName, setCityName] = useState('London');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          'https://jsonplaceholder.typicode.com/users',
-        );
+        const response = await axios.get(url, {
+          headers: {
+            'X-RapidAPI-Key': Config.API_KEY,
+            'X-RapidAPI-Host': Config.API_HOST,
+          },
+          params: {
+            city: cityName,
+          },
+        });
         setData(response.data);
       } catch (e) {
         setError(e);
@@ -21,9 +29,9 @@ const useFetch = url => {
       }
     };
     fetchData();
-  }, [url]);
+  }, [url, cityName]);
 
-  return {data, loading, error};
+  return {data, loading, error, setCityName};
 };
 
 export default useFetch;
